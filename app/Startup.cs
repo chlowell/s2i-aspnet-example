e773488d0ab2eea4619fbda810ca1a288002e5c0
Charles Lowell <chlowe@microsoft.com>
2016-10-07 14:55:11 -0700

    Try SapientGuardian MySql provider

--- app/Startup.cs
+++ app/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using MySQL.Data.EntityFrameworkCore.Extensions;
+using MySQL.Data.Entity.Extensions;
 using WebApplication.Data;
 using WebApplication.Models;
 using WebApplication.Services;
